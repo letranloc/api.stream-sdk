ca72cc53a238e6a8b2922cda91a77c344e78a1f4
ty-lightstream <85856985+ty-lightstream@users.noreply.github.com>
2022-05-13 15:44:07 +0000

    Deploying to gh-pages from @ golightstream/api.stream-sdk@355d2f27f63636c0a6ef9fdf99d93e462fb7b43f 🚀

--- sdk/java/src/main/stream/api/utils/AccessTokenRefreshCallback.java
+++
@@ -1,9 +0,0 @@
-/* ---------------------------------------------------------------------------------------------
- * Copyright (c) Infiniscene, Inc. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * -------------------------------------------------------------------------------------------- */
-package stream.api.utils;
-
-public interface AccessTokenRefreshCallback {
-    void accessTokenRefreshCallback(String accessToke);
-}
